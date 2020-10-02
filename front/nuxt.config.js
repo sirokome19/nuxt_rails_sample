d9c1003fbad11fbdb8253039fddbfbba3b00c7a1
sirokome19 <sirokome19@toki.waseda.jp>
2020-10-02 21:38:48 +0900

    GET user

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -38,7 +38,11 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/axios'
   ],
+  axios: {
+
+  },
   /*
   ** Build configuration
   */
@@ -46,7 +50,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
---
+++ front/pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello, {{ name }}</h1>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(`http://back:3000/users/${params.id}`);
+    return { name: data.name };
+  },
+};
+</script>
