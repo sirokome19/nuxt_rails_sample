97d04d5cd3b093c7138f26d94aba0d4be02bcbed
sirokome19 <sirokome19@toki.waseda.jp>
2020-11-19 18:28:08 +0900

    get and post answer

--- back/app/controllers/answer_sheets_controller.rb
+++ back/app/controllers/answer_sheets_controller.rb
@@ -3,23 +3,59 @@ class AnswerSheetsController < ApplicationController
 
   def index
     answer_sheets = AnswerSheet.all()
-    render json: { status: 'SUCCESS', message: 'Loaded answer_sheets', data: answer_sheets }
+    render json:answer_sheets
   end
 
   def show
     # show questions(questionnaire detail)
-    render json: { status: 'SUCCESS', message: 'Loaded the answer_sheet', data: @questionnaire }
+    render json: @answer_sheet.as_json(
+      include: {
+        order_answers: {
+          only: [:order_num],
+          include:{
+            choice_answer:{
+              include: {
+                option:{
+                  only:[:text, :image_url],
+                  include:{
+                    choice_question:{
+                      only:[:text]
+                    }
+                  }
+                }
+              }
+            },
+            free_answer:{
+              only:[:text],
+              include: {
+                free_question:{
+                  only:[:text]
+                }
+              }
+            }
+          }
+        }
+      }
+    )
   end
 
   def create
     # answer question
-
-    # post = Post.new(post_params)
-    # if post.save
-    #   render json: { status: 'SUCCESS', data: post }
-    # else
-    #   render json: { status: 'ERROR', data: post.errors }
-    # end
+    ActiveRecord::Base.transaction do
+      answer_sheet=AnswerSheet.create!(answer_sheet_params)
+      order_answers_params.require(:order_answers).each_with_index do |answer_params, idx|
+        order_answer=answer_sheet.order_answers.create!(order_num: idx)
+        case answer_params.require(:type)
+        when "ChoiceAnswer" then
+          answer=order_answer.create_choice_answer(option_id: answer_params.require(:option_id))
+        when "FreeAnswer" then
+          print(answer_params.require(:free_question_id))
+          order_answer.create_free_answer!(text: answer_params.require(:text), free_question_id: answer_params.require(:free_question_id))
+        else
+          raise RuntimeError, "undefined answer type error"
+        end
+      end
+    end
   end
 
   def destroy
@@ -39,10 +75,12 @@ class AnswerSheetsController < ApplicationController
 
   def set_post
     @answer_sheet = AnswerSheet.find(params[:id])
-    @questionnaire = @answer_sheet.questionnaire
   end
 
-  def post_params
-    params.require(:post).permit(:title)
+  def answer_sheet_params
+    params.require(:answer_sheet).permit(:user_id, :questionnaire_id)
+  end
+  def order_answers_params
+    params.require(:answer_sheet).permit(order_answers: [:type, :text, :option_id, :free_question_id])
   end
 end
